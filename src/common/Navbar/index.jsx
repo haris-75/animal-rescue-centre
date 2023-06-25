675c6db2d5b4ce5f7a8a1f8190a85d94f76625d7
haris-75 <haris.ejaz@datumbrain.com>
2023-06-25 18:04:24 +0500

    Make Mobile Nav Items

--- src/common/Navbar/index.jsx
+++ src/common/Navbar/index.jsx
@@ -3,34 +3,86 @@ import Button from '../../components/Button';
 import HeartIcon from '../../assets/heart-img.svg';
 import PhoneIcon from '../../assets/phone.svg';
 import MobileToggler from '../../assets/mobile-nav-toggle.svg';
+import Cross from '../../assets/cross.svg';
+import { useState } from 'react';
 export default function Navbar() {
+  const [showMobileNav, setShowMobileNav] = useState(false);
   return (
-    <div className='flex sticky top-0 justify-between py-6 border-b-2 border-dark bg-white-secondary xl:px-[100px] lg:px-[60px] md:px-6 sm:px-10 px-8'>
-      {/* Nav Left */}
-      <div className='flex lg:gap-4 md:gap-2'>
-        <div className='p-1.5'>
-          <img
-            width={40}
-            src={Logo}
-            alt='logo'
-          />
+    <>
+      <div className='flex sticky top-0 justify-between py-6 border-b-2 border-dark backdrop-blur-lg xl:px-[100px] lg:px-[60px] md:px-6 sm:px-10 px-8'>
+        {/* Nav Left */}
+        <div className='flex lg:gap-4 md:gap-2'>
+          <div className='p-1.5'>
+            <img
+              width={40}
+              src={Logo}
+              alt='logo'
+            />
+          </div>
+          <ul className='md:flex xl:gap-5 md-lg:gap-3 md:gap-2 text-dark font-medium xl:text-lg md-lg:text-base md:text-xs items-center hidden'>
+            <li>Rescue center</li>
+            <li>Apply to adopt</li>
+            <li>Volunteer</li>
+            <li>Ways to give</li>
+            <li>Events</li>
+          </ul>
         </div>
-        <ul className='md:flex xl:gap-5 md-lg:gap-3 md:gap-2 text-dark font-medium xl:text-lg md-lg:text-base md:text-xs items-center hidden'>
-          <li>Rescue center</li>
-          <li>Apply to adopt</li>
-          <li>Volunteer</li>
-          <li>Ways to give</li>
-          <li>Events</li>
-        </ul>
-        <div className='md:hidden hidden sm:block w-[40px] m-auto ml-4'>
+        {/* Nav Right */}
+        <div className='md:flex items-center xl:gap-8 lg:gap-5 md:gap-3 md:flex-row flex-col-reverse gap-2 hidden'>
+          <div className='items-center flex gap-3'>
+            <div className='md:w-[12px]'>
+              <img
+                src={PhoneIcon}
+                alt='phone-icon'
+              />
+            </div>
+            <span className='md:text-sm'>631-249-7325</span>
+          </div>
+          <div>
+            <Button
+              icon={HeartIcon}
+              text='Donate'
+              backgroundColor='bg-primary'
+              textColor='text-white'
+            />
+          </div>
+        </div>
+        {/* Mobile Nav Icon */}
+        <div
+          className='md:hidden block w-[40px] my-auto'
+          onClick={() => setShowMobileNav(!showMobileNav)}
+        >
           <img
+            className={`${showMobileNav ? 'hidden' : ''}`}
             src={MobileToggler}
             alt='mobile-nav-toggler'
           />
+          <img
+            className={`m-auto ${showMobileNav ? '' : 'hidden'}`}
+            src={Cross}
+            width={30}
+            alt='mobile-nav-toggler'
+          />
         </div>
+        {/* Mobile Menu */}
+        <ul
+          className={`w-full pb-9 px-9 absolute ${
+            showMobileNav ? 'block' : 'hidden'
+          } md:hidden bg-white-secondary top-[102px] left-0`}
+        >
+          {['Home', 'About', 'Contact', 'FAQs', 'Testimonials']?.map((ele) => (
+            <li key={ele}>
+              <div>
+                <div className='inline-block cursor-pointer pt-9 font-secondary-font text-md capitalize transition-all '>
+                  {ele}
+                </div>
+              </div>
+            </li>
+          ))}
+        </ul>
       </div>
-      {/* Nav Right */}
-      <div className='sm:flex items-center xl:gap-8 lg:gap-5 sm:gap-3 sm:flex-row flex-col-reverse gap-2 hidden'>
+      {/* Mobile Nav Bottom */}
+      <div className='w-full justify-evenly p-6 flex fixed items-center gap-3 md:hidden bottom-0 backdrop-blur-lg'>
         <div className='items-center flex gap-3'>
           <div className='md:w-[12px]'>
             <img
@@ -49,50 +101,6 @@ export default function Navbar() {
           />
         </div>
       </div>
-      <div className='md:hidden sm:hidden block w-[40px] my-auto'>
-        <img
-          src={MobileToggler}
-          alt='mobile-nav-toggler'
-        />
-      </div>
-      {/* Mobile Menu */}
-      {/* <ul className='mobile-menu hidden md:flex md:gap-12 md:text-sm lg:text-base'>
-        <li className='relative'>
-          <div>
-            <div className='inline-block cursor-pointer py-9 font-secondary-font text-sm capitalize transition-all  nav_active font-bold'>
-              Home
-            </div>
-          </div>
-        </li>
-        <li className='relative'>
-          <div>
-            <div className='inline-block cursor-pointer py-9 font-secondary-font text-sm capitalize transition-all '>
-              About
-            </div>
-          </div>
-        </li>
-        <li className='relative'>
-          <div>
-            <div className='inline-block cursor-pointer py-9 font-secondary-font text-sm capitalize transition-all '>
-              Reviews
-            </div>
-          </div>
-        </li>
-        <li className='relative'>
-          <div>
-            <div className='inline-block cursor-pointer py-9 font-secondary-font text-sm capitalize transition-all '>
-              Contact
-            </div>
-          </div>
-        </li>
-        <li className='relative'>
-          <div>
-            <div className='inline-block cursor-pointer py-9 font-secondary-font text-sm capitalize transition-all '>
-              Blog
-            </div>
-          </div>
-        </li>
-      </ul> */}
-    </div>
+    </>
   );
 }
